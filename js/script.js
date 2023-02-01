2653e12f2f6c491a640659b93b2f6ff36d0feeff
ariel romero <ar039980@gmail.com>
2023-02-01 17:31:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+ 
+ <script>
+ function mostrar(){
+     document.getElementById("img").style.display = "block"
+ }
+
+ function ocultar(){
+     document.getElementById("img").style.display = "none"
+ }
+</script>
+
